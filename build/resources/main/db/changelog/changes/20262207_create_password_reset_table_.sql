454a2ed2d33a3cd7ab0450e00c347098036a02b4
Moses Nwaeze <moses.nwaeze@fcmb.com>
2026-07-22 14:21:29 +0100

    committing changes

--- build/resources/main/db/changelog/changes/20262207_create_password_reset_table_.sql
+++ build/resources/main/db/changelog/changes/20262207_create_password_reset_table_.sql
@@ -2,7 +2,7 @@
 -- changeset moses.nwaeze:20262207_create_password_reset_table_.sql
 CREATE TABLE password_reset_tokens
 (
-    id            BIGSERIAL PRIMARY KEY,
+    id            UUID      DEFAULT gen_random_uuid() PRIMARY KEY,
     token         VARCHAR(255) NOT NULL,
     expires_at    TIMESTAMP    NOT NULL,
     user_login_id UUID         NOT NULL,
